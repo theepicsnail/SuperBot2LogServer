959530bbbc7d1cdd4023763801248dd24420cfcc
Steven <theepicsnail@gmail.com>
2011-06-14 01:42:01 -0400

    started adding API functions

--- LogServer.py
+++ LogServer.py
@@ -4,22 +4,27 @@ import json
 import os
 import urlparse
 import re
-def processLogs(**options):
+
+history = [] # [[date,pid,level,pos,msg,fname],...]
+            
+
+def cacheLogs():
     #get list of file
-    entries = []
+    global history
+    history=[]
+    
     for cur,dirs,files in os.walk(os.path.join("..","Logs")):
         for fname in files:
-            r = options.get("FileName",".*")
-            if re.search(r,fname):
-                for line in  file(os.path.join(cur,fname)):
-                    date,pid,level,pos,msg = line.split("|",4)
-                    # process stuff against **options
-                    entries.append([date,pid,level,pos,msg,fname])
+            for line in  file(os.path.join(cur,fname)):
+                date,pid,level,pos,msg = line.split("|",4)
+                history.append([date,pid,level,pos,msg,fname])
             #2011-06-10 16:11:44,043|136320734516992|10|Core:<module>:172|End of core\n
     
     #construct the ordered version 
-    entries.sort()
+    history.sort()
+cacheLogs()
 
+def processLogs(**options):
     # <div class="log %i">
     #     <span class="date">2011-06-10 10:15:58,477</span>
     #     <span class="pid">107322765797120</span>
@@ -30,19 +35,76 @@ def processLogs(**options):
     #           <span>This is another line in the message</span>
     #     </span>
     # </span>
+    ops = {
+        "maxid":-1,
+        "minid":0,
+        "before":"x",
+        "after":"",
+        "pid":"",
+        "minlevel":0,
+        "maxlevel":50,
+        "msg":".*",
+        "file":".*"}
+    expect = map(lambda (x,y):(x,type(y)),ops.items())
+    ops.update(options)#overwrite defaults with whatevers provided
+    try:
+        print expect
+        for k,t in expect:
+            ops[k]=t(ops[k])       
+        print "\n\t".join(map(str,ops.items()))
+    except:
+        print "Failed sanity check."
+        return "[]"
+
+
     out = []
-    
-    for no,line in enumerate(entries):
-        e = {}
-        for n,val in enumerate(line):
-            key = ["date","pid","level","line","msg","file"][n]
-            if n == 4:
-                e[key]=map(cgi.escape,val.split("\t"))
-            elif n ==3:
-                e[key]=map(cgi.escape,val.split(":"))
-            else:
-                e[key]=cgi.escape(val)
-        out.append(e)
+    global history
+    for no,line in enumerate(history[ops["minid"]:ops["maxid"]]):
+        no = no + ops["minid"]
+        try:
+            e = {"id":no}
+            for n,val in enumerate(line):
+                key = ["date","pid","level","line","msg","file"][n]
+                if   n==5:
+                    if re.search(ops["file"],val):
+                        e[key]=val
+                    else:
+                        e=None
+                elif n==4:
+                    if re.search(ops["msg"],val):
+                        e[key]=map(cgi.escape,val.split("\t"))
+                    else:
+                        e = None
+                elif n==3:
+                    if re.search(ops["file"],val):
+                        e[key]=map(cgi.escape,val.split(":"))
+                    else:
+                        e = None
+                elif n==2:
+                    if ops["minlevel"]<int(val)<ops["maxlevel"]:
+                        e[key]=int(val)
+                    else:
+                        e = None
+                elif n==1:
+                    if not ops["pid"] or ops["pid"]==val:
+                        e[key]=cgi.escape(val)
+                    else:
+                        e = None
+                else:
+                    print ops["after"],"--<--",val,"--<--",ops["before"]
+                    if ops["after"]<val<ops["before"]:
+                        e[key]=cgi.escape(val)
+                    else:
+                        e = None
+
+                if e == None:
+                    break
+            if e:
+                out.append(e)
+        except:
+            print "Error parsing line:"
+            print no,line
+            pass
     #in the future this version should cache the output, and timestamps, only recomputing as necessary
     return json.dumps(out)
 
